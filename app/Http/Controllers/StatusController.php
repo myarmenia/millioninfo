93e76839bc206af5397815b9242327bd5976c7c1
Hayk Arakelyan <info@webex.am>
2022-02-15 09:47:24 +0400

    Update StatusController.php

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Status;
 use App\Models\Categorie;
+use App\Models\Categories;
+use App\Models\SubCategories;
 
 class StatusController extends Controller
 {
@@ -15,11 +17,16 @@ class StatusController extends Controller
     }
     public function index()
     {  
+        
         $data = Categories::all();
-         $data2 = SubCategories::all();
+        $data2 = SubCategories::all();
 
-         return view('status.index',compact('data','data2'));
+        return view('status.index',compact('data','data2'));
     }
+
+
+
+
      public function create(Request $request)
     {   
         $this->validate($request, [
@@ -29,18 +36,32 @@ class StatusController extends Controller
         ]);
 
         $catigory= json_encode($request->only('en','hy','ru'));
-       
-      echo  $insert = Status::create([
-        'name'=>$request['en'],'catigory'=>$catigory]);
+      
+       $insert = SubCategories::create([
+        'categories_id'=>$request->categories_id,
+        'name'=>$catigory,
+    ]);
    
         return  redirect()->back();
     }
+
+    
     public function delete($id)
     {   
         
-        $delete = Status::where('id',$id)->delete();
+        $delete = SubCategories::where('id',$id)->delete();
 
-      return  redirect()->back();
+        return  redirect()->back();
+
+       
+    }
+
+    public function indexedit(){
+
+        return view('status.subedit');
+
+        
+
     }
 
     
