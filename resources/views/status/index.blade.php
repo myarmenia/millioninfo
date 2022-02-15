fcbb10c21e221ad41c1868932b2edca4f9908cc3
Hayk Arakelyan <info@webex.am>
2022-02-15 09:44:50 +0400

    Update index.blade.php

--- resources/views/status/index.blade.php
+++ resources/views/status/index.blade.php
@@ -5,7 +5,7 @@
 <div class="row">
     <div class="col-lg-12 margin-tb">
         <div class="pull-left">
-            <h2>Status</h2>
+            <h2>Sub Categories</h2>
         </div>
     </div>
 </div>
@@ -20,67 +20,71 @@
 
     <label for="cars">Choose Categories</label>
     <select name="categories_id" id="cars">
-          @foreach ($data as $info )
-            <option value="1">{{$info->name}}</option>
+        @foreach ($data as $info )
+            <option value="{{$info->id}}">{{$info->name}}</option>
         @endforeach
-    4</option>
     </select>
 
     <input type="submit" name="sent" value="Ավելացնել">
 </form>
 
 
+
 <table class="table table-bordered">
  <tr>
-   <th>No</th>
+   <th>ID</th>
+   <th>categories_id</th>
    <th>Name</th>
    <th>Action</th>
    {{-- <th >Translet json</th>
    <th >Filter</th>
    <th >Action</th> --}}
  </tr>
- @foreach ($status as $data)
   <tr>
-    <td>{{ $data->id }}</td>
-    <td>{{ $data->name }}</td>
-    <td>
-        <a href={{"show/".$data['id']}}>Edit</a>
-    </td>
-    </td>
-     {{-- <td><input type="text" onchange="sentvalue(this.value,{{$data->id}})" value="{{$data->filter}}" name=""></td> --}}
-    <td>
-       {{-- <a class="btn btn-info" href="statusdelete/{{$data->id}}">Delete</a> --}}
-       {{-- <script>
+    @foreach ($data2 as $info2)
+        <td>{{$info2->id}}</td>
+        <td>{{$info2->categories_id}}</td>
+        <td>{{$info2->name}}</td>
+        <td>
+            <a href="/index"><button type="button" class="btn btn-primary">Edit</button></a>
+            <a href={{"statusdelete/".$info2['id']}}><button type="button" class="btn btn-primary">Delete</button></a>
+        </td>
+        </td>
+        {{-- <td><input type="text" onchange="sentvalue(this.value,{{$data->id}})" value="{{$data->filter}}" name=""></td> --}}
+        <td>
+        {{-- <a class="btn btn-info" href="statusdelete/{{$data->id}}">Delete</a> --}}
+        {{-- <script>
 
-                      function sentvalue(value,id){
+                        function sentvalue(value,id){
 
-                        $.ajaxSetup({
+                            $.ajaxSetup({
 
-                            headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});$.ajax({type:'POST',url:"{{ '/updatefiter'}}",data:{id:id, value:value},success: function(data,id){
+                                headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});$.ajax({type:'POST',url:"{{ '/updatefiter'}}",data:{id:id, value:value},success: function(data,id){
 
-                                if(data == "price"){
+                                    if(data == "price"){
 
-                                    location.reload();
+                                        location.reload();
 
-                                }else{
+                                    }else{
 
-                                    location.reload();
+                                        location.reload();
+
+                                    }
 
                                 }
 
+                            });
+
                             }
 
-                        });
-
-                        }
-
-                    </script> --}}
-    </td>
+                        </script> --}}
+        </td>
+    @endforeach
   </tr>
- @endforeach
+
 </table>
-<div class="pagination-newm">
+{{-- <div class="pagination-newm">
     {{$status->links()}}
-</div>
+</div> --}}
 <p class="text-center text-primary"><small>Webex.am</small></p>
 @endsection
